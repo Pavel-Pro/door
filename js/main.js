f9e105b3f25d2dd7e80680c8a5def91ce741f895
Pavel-Pro <proskpavel@gmail.com>
2021-05-08 13:38:35 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -28,28 +28,6 @@
 			$('.header').removeClass('fixed');
 		}
 	});
-	// $('.header-menu__item.dropdown').on('mouseover', function() {
-	// 	console.log('mouseover');
-	// 	$(this).children('.sub-menu__inner').show();
-	// });
-	// $('.header-menu__item.dropdown').on('mouseout', function() {
-	// 	// $(this).children('.sub-menu__inner').delay(7000).removeClass('hover');
-	// 	console.log('mouseout');
-	// 	// $(this).children('.sub-menu__inner').delay(1000).hide();
-	// 	function hideFunc() {
-	// 		console.log('setTimeout');
-	// 		$('.header-menu__item.dropdown').children('.sub-menu__inner').hide()
-	// 	}
-	// 	setTimeout(hideFunc, 5000);
-	// });
-	// $('.sub-menu__inner').on('mouseover', function() {
-	// 	console.log('mouseover');
-	// 	$(this).show();
-	// });
-	// $('.sub-menu__inner').on('mouseout', function() {
-	// 	console.log('mouseout');
-	// 	$(this).hide();
-	// });
 	$('.header-menu__item.dropdown').hover(function(){
 		if($(this).children('.sub-menu__inner').is(':visible')){
 			$(this).children('.sub-menu__inner').addClass('sub-menu__inner_opacity');
